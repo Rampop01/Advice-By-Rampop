dcbc4ccccbbb1594aa2b28616199a4cbaf1247f7
Rampop01 <popoolarahmat96@gmail.com>
2024-01-03 00:39:53 -0800

    axios api practice using advice api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+    return (
+      <div>
+        <div className=" h-[100vh] flex items-center justify-center text-center bg-gradient-to-r from-green-400 to-blue-500">
+          <div className=" w-full bg-white max-w-[540px] mx-auto my-20 p-10 rounded-2xl shadow-inner shadow-black  ">
+            <h1 className="italic text-2xl text-w font-bold">{advice}</h1>
+            <button
+              onClick={this.fetchAdvice}
+              className="bg rounded-full border border-black px-6 py-2 mt-10 font-bold bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500  "
+            >
+              GET ADVICE
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
